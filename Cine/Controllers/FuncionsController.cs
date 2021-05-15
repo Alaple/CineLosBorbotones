3a325cfe8ec8c9bde9066e9c1250fc91f29d2790
Juan Pablo Ocampo <juampioca@gmail.com>
2021-05-14 21:55:16 -0300

    controllers not working yet, but build

---
+++ Cine/Controllers/FuncionsController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Cine.Models;
+
+namespace Cine.Controllers
+{
+    public class FuncionsController : Controller
+    {
+        private readonly CineContext _context;
+
+        public FuncionsController(CineContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Funcions
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Funciones.ToListAsync());
+        }
+
+        // GET: Funcions/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var funcion = await _context.Funciones
+                .FirstOrDefaultAsync(m => m.funcionID == id);
+            if (funcion == null)
+            {
+                return NotFound();
+            }
+
+            return View(funcion);
+        }
+
+        // GET: Funcions/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Funcions/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("funcionID,dia,hora,cantButacasLibres")] Funcion funcion)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(funcion);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(funcion);
+        }
+
+        // GET: Funcions/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var funcion = await _context.Funciones.FindAsync(id);
+            if (funcion == null)
+            {
+                return NotFound();
+            }
+            return View(funcion);
+        }
+
+        // POST: Funcions/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("funcionID,dia,hora,cantButacasLibres")] Funcion funcion)
+        {
+            if (id != funcion.funcionID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(funcion);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FuncionExists(funcion.funcionID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(funcion);
+        }
+
+        // GET: Funcions/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var funcion = await _context.Funciones
+                .FirstOrDefaultAsync(m => m.funcionID == id);
+            if (funcion == null)
+            {
+                return NotFound();
+            }
+
+            return View(funcion);
+        }
+
+        // POST: Funcions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var funcion = await _context.Funciones.FindAsync(id);
+            _context.Funciones.Remove(funcion);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FuncionExists(int id)
+        {
+            return _context.Funciones.Any(e => e.funcionID == id);
+        }
+    }
+}
--- Cine/Models/CineContext.cs
+++ Cine/Models/CineContext.cs
@@ -14,10 +14,17 @@ namespace Cine.Models
         }
         //Error de instancia de DBContext constructor parametrizado sigue en Startup.cs  linea 29
         public DbSet<Cine> Cines { get; set; }
-       
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<Sala> Salas { get; set; }
+        public DbSet<Pelicula> Peliculas { get; set; }
+        public DbSet<Funcion> Funciones { get; set; }
+        
+
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=xxxx;Database=CineDB;Trusted_Connection=True;");
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-OUG6EEB\SQLEXPRESS;Database=CineDB;Trusted_Connection=True;");
         }
     }
 }
